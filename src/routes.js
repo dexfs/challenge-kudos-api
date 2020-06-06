55a3ba94281db539fc9b70aac4ebd819429ec376
Andre Santos <andre@dexdev.com.br>
2020-06-06 03:25:18 -0300

    feat(routes): add routes skeleton

--- src/routes.js
+++ src/routes.js
@@ -5,8 +5,19 @@ import IndexController from './app/controllers/IndexController';
 
 const routes = new Router();
 
+routes.get('/kudos/all', (req, res) => {
+  return res.json({ data: 'ok' });
+});
+routes.get('/kudos/chart', (req, res) => {
+  return res.json({ data: 'ok' });
+});
+routes.get('/users/chart', (req, res) => {
+  return res.json({ data: 'ok' });
+});
+
+// post
 routes.post(
-  '/',
+  '/me',
   celebrate({
     [Segments.BODY]: Joi.object({
       owner: Joi.string().required(),
@@ -15,5 +26,8 @@ routes.post(
   }),
   IndexController.index
 );
+routes.post('/kudos', (req, res) => {
+  return res.status(201).json({ data: req.body });
+});
 
 export default routes;
