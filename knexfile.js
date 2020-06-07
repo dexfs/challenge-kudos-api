b56892ad910fccd36dd400ae61fc799a307ad690
Andre Santos <andre@dexdev.com.br>
2020-06-07 00:19:04 -0300

    refactor(config): change test config from sqlite to postgres

--- knexfile.js
+++ knexfile.js
@@ -1,17 +1,27 @@
-require('dotenv-flow').config();
+require('dotenv-flow').config({
+  silent: true,
+});
 const path = require('path');
 module.exports = {
   test: {
-    client: 'sqlite3',
-    connection: {
-      filename: './dev.sqlite3',
+    client: 'postgresql',
+    connection: process.env.DB_DSN || {
+      port: process.env.PORT || '5432',
+      host: process.env.DB_HOST || '127.0.0.1',
+      database: process.env.DB_NAME,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+    },
+    pool: {
+      min: 2,
+      max: 10,
     },
     migrations: {
-      directory: path.resolve('src', 'database', 'migrations'),
+      directory: path.resolve(__dirname, 'src', 'database', 'migrations'),
       tableName: 'knex_migrations',
     },
     seeds: {
-      directory: path.resolve('src', 'database', 'seeds'),
+      directory: path.resolve(__dirname, 'src', 'database', 'seeds'),
     },
   },
 
