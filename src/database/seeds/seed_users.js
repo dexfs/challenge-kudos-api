e10fcca699dc1c5eb68e038e660e95928f437ea3
Andre Santos <andre@dexdev.com.br>
2020-06-07 00:00:47 -0300

    fix(seed): users

--- src/database/seeds/seed_users.js
+++ src/database/seeds/seed_users.js
@@ -1,13 +1,18 @@
 const { generateUsers, generateUserKudos } = require('./../fixtures/users');
+const usersSeed = generateUsers();
 exports.seed = function (knex) {
   // Deletes ALL existing entries
   return knex('users')
     .del()
     .then(function () {
       // Inserts seed entries
-      return knex('users').insert(generateUsers());
+      return knex('users').insert(usersSeed);
     })
-    .then(function () {
-      return knex('user_kudos').insert(generateUserKudos());
+    .then(function (result) {
+      knex('user_kudos')
+        .del()
+        .then(function () {
+          return knex('user_kudos').insert(generateUserKudos(usersSeed));
+        });
     });
 };
