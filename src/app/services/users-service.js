d376db6f9904bf770a33e50aa4673afc1aa81024
Andre Santos <andre@dexdev.com.br>
2020-06-08 01:09:14 -0300

    feat: users service

--- src/app/services/users-service.js
+++ src/app/services/users-service.js
@@ -1,7 +1,19 @@
 import { knex } from '../../config';
+import groupBy from 'lodash/groupBy';
 
-export const all = async () => {
-  return await knex('users');
+export const all = async (user) => {
+  console.log({ user });
+  try {
+    const users = await knex('users').whereNot(
+      'users.id',
+      '=',
+      Number(user.id)
+    );
+
+    return users;
+  } catch (error) {
+    throw error;
+  }
 };
 
 export const findOrCreate = async (data) => {
@@ -14,7 +26,11 @@ export const findOrCreate = async (data) => {
     access_token = '',
   } = data;
 
-  let user = await knex('users').where({ id }).first('*');
+  if (!id) throw new Error('Não foi possível buscar ou criar o usuário');
+
+  let user = await knex('users')
+    .where('id', '=', Number(data.id))
+    .first(['id', 'name', 'login', 'avatar_url', 'github_profile']);
   if (user) {
     return user;
   }
@@ -31,4 +47,19 @@ export const findOrCreate = async (data) => {
     });
 
   return user;
+};
+
+export const me = async (id) => {
+  const user = await knex('users').where('users.id', '=', Number(id)).first();
+  const userKudos = await knex
+    .select(
+      'user_kudos.quantity',
+      'user_kudos.kudo_id',
+      'kudos.name as kudo_name'
+    )
+    .from('user_kudos')
+    .where('user_kudos.user_id', '=', Number(id))
+    .joinRaw('INNER JOIN kudos ON kudos.id::text = user_kudos.kudo_id::text');
+
+  return { ...user, userKudos };
 };
